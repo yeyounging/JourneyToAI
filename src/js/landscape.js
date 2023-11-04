e6a55f4be2312191896c449db1643e237173f41c
SungHoon Jung <32007781+wooing1084@users.noreply.github.com>
2023-11-04 21:49:53 +0900

    ADD: Lights

--- src/js/landscape.js
+++ src/js/landscape.js
@@ -31,31 +31,65 @@ class App {
         this._contols = new OrbitControls(this._camera, this._divContainer);
     }
 
+    // =========== Camera ===========
     // Scene의 카메라 설정
     _setupCamera() {
         const width = this._divContainer.clientWidth;
         const height = this._divContainer.clientHeight;
-        const camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 100);
-        camera.position.z = 2;
+        const camera = new THREE.PerspectiveCamera(60, width / height, 1, 5000);
+        camera.position.set(0, 100, 500);
         this._camera = camera;
     }
 
+    // =========== Light ===========
     // Scene의 빛 설정
     _setupLight() {
-        const color = 0xFFFFFF;
-        const intensity = 1;
-        const light = new THREE.DirectionalLight(color, intensity);
-        light.position.set(-1, 2, 4);
-        this._scene.add(light);
+        const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
+        this._scene.add(ambientLight);
+
+        this._addPointLight(500, 150, 500, 0xffffff);
+        this._addPointLight(-500, 150, 500, 0xffffff);
+        this._addPointLight(-500, 150, -500, 0xffffff);
+        this._addPointLight(500, 150, -500, 0xffffff);
+
+        const shadowLight = new THREE.DirectionalLight(0xffffff, 0.2);
+        shadowLight.position.set(200, 500, 200);
+        shadowLight.target.position.set(0, 0, 0);
+        const directionalLightHelper = new THREE.DirectionalLightHelper(shadowLight, 10);
+        this._scene.add(directionalLightHelper);
+
+        this._scene.add(shadowLight);
+        this._scene.add(shadowLight.target);
     }
 
+    // Scene에 PointLight 추가
+    _addPointLight(x, y, z, color, helperColor = 0xffffff, intensity = 1.5, distance = 2000) {
+        const pointLight = new THREE.PointLight(color, 1.5, 2000);
+        pointLight.position.set(x, y, z);
+
+        this._scene.add(pointLight);
+
+        const pointLightHelper = new THREE.PointLightHelper(pointLight, 10, helperColor);
+        this._scene.add(pointLightHelper);
+    }
+
+
+    // =========== Model ===========
     // Scene에 모델 추가
     _setupModel() {
+        const planeGeometry = new THREE.PlaneGeometry(1000, 1000);
+        const planeMaterial = new THREE.MeshPhongMaterial({ color: 0x878787 });
+        const plane = new THREE.Mesh(planeGeometry, planeMaterial);
+        plane.rotation.x = -Math.PI / 2;
+        this._scene.add(plane);
+
         new GLTFLoader().load("../../assets/models/character.glb", (gltf) => {
             const model = gltf.scene;
             this._scene.add(model);
         });
     }
+
+    // =========== For Updates ===========
 
     // 화면 크기 재조정
     resize() {
@@ -77,6 +111,8 @@ class App {
     // 실시간 갱신되는 정보나, 애니메이션 적용하는데 사용
     update(time) {
         time *= 0.001; // ms -> s
+
+        this._contols.update();
     }
 }
 
